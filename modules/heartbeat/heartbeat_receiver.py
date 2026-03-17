02bd15b5c85b34d0ea950b5af969ee57b0c20633
chris197128 <cb.mcquillan23@gmail.com>
2026-03-17 11:40:59 -0400

    receiver completed

--- modules/heartbeat/heartbeat_receiver.py
+++ modules/heartbeat/heartbeat_receiver.py
@@ -21,34 +21,76 @@ class HeartbeatReceiver:
     def create(
         cls,
         connection: mavutil.mavfile,
-        args,  # Put your own arguments here
-        local_logger: logger.Logger,
-    ):
+          # Put your own arguments here
+        local_logger: logger.Logger
+    ) -> "tuple[True, HeartbeatReceiver] | tuple[False, None]":
+        
         """
         Falliable create (instantiation) method to create a HeartbeatReceiver object.
         """
-        pass  # Create a HeartbeatReceiver object
+        
+        try:
+            instance = cls(
+                cls.__private_key,
+                connection,
+                local_logger
+            )
+        except Exception as e:
+            local_logger.error("Failed to create HeartbeatReceiver instance: " + str(e))
+            return False, None
+        else:
+            local_logger.debug("HeartbeatReceiver instance created", True)
+            return True, instance
+
+        # Create a HeartbeatReceiver object
 
     def __init__(
         self,
         key: object,
         connection: mavutil.mavfile,
-        args,  # Put your own arguments here
+        local_logger: logger.Logger,
+        missed = 0,
+        state = "Disconnected" # Put your own arguments here
     ) -> None:
         assert key is HeartbeatReceiver.__private_key, "Use create() method"
 
+        self.connection = connection
+        self.local_logger = local_logger
+        self.missed = missed
+        self.state = state
         # Do any intializiation here
 
     def run(
         self,
-        args,  # Put your own arguments here
+        period,
+        error,
+        threshold  # Put your own arguments here
     ):
         """
         Attempt to recieve a heartbeat message.
         If disconnected for over a threshold number of periods,
         the connection is considered disconnected.
         """
-        pass
+        
+
+        msg = self.connection.recv_match(type="HEARTBEAT", blocking=True, timeout= period + error)
+        
+        if msg:
+            self.missed = 0
+            self.state = "Connected"
+            self.local_logger.debug("Connected")
+        else:
+            self.missed += 1
+            self.local_logger.warning("Missed Heartbeats: " + str(self.missed))
+        
+        if self.missed >= threshold:
+            self.state = "Disconnected"
+            self.local_logger.warning("Disconnected")
+        
+        return self.state
+        
+            
+        
 
 
 # =================================================================================================
--- modules/heartbeat/heartbeat_receiver_worker.py
+++ modules/heartbeat/heartbeat_receiver_worker.py
@@ -18,7 +18,12 @@ from ..common.modules.logger import logger
 # =================================================================================================
 def heartbeat_receiver_worker(
     connection: mavutil.mavfile,
-    args,  # Place your own arguments here
+    controller: worker_controller.WorkerController,
+    state_queue_wrapper,
+    period = 1,
+    error = 1e-2,
+    threshold = 5
+    # Place your own arguments here
     # Add other necessary worker arguments here
 ) -> None:
     """
@@ -47,8 +52,27 @@ def heartbeat_receiver_worker(
     #                          ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
     # =============================================================================================
     # Instantiate class object (heartbeat_receiver.HeartbeatReceiver)
+    success, receiver = heartbeat_receiver.HeartbeatReceiver.create(
+        connection, local_logger)
+
+
+    if not success:
+        local_logger.error("Failed to create Heartbeat receiver")
+        return
+    
+    local_logger.info("Heartbeat receiver started", True)
+
 
     # Main loop: do work.
+
+    while not controller.is_exit_requested():
+        try:
+            state = receiver.run(period, error, threshold)
+            state_queue_wrapper.queue.put(state)
+        except Exception as e:
+            local_logger.error("Run function failed: " + str(e))
+    
+    local_logger.info("Heartbeat receiver stopped")
 
 
 # =================================================================================================
